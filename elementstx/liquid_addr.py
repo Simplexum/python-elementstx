1e7426cc3d416a7a57fd4c650b029d559e31623b
Dmitry Petukhov <dp@simplexum.com>
2019-09-13 14:13:52 +0500

    update docstring

--- elementstx/liquid_addr.py
+++ elementstx/liquid_addr.py
@@ -104,7 +104,9 @@ def convertbits(data, frombits, tobits, pad=True):
 
 
 def decode(hrp, addr):
-    """Decode a segwit address."""
+    """Decode a segwit confidential address.
+    It is longer than unconfidential address by 33 bytes
+    (the length of blinding pubkey)"""
     hrpgot, data = blech32_decode(addr)
     if hrpgot != hrp:
         return (None, None)
